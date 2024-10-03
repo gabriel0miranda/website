1f4fb37fda4b551c77cb740582643d96b6f41c6c
Gabriel Miranda <gabriel.miranda@discente.ufg.br>
2024-10-03 18:42:04 -0300

    WIP: trabalhando na homepage, agora o redirecionamento funciona corretamente usando o script de inicialização

--- init-script.sh
+++ init-script.sh
@@ -3,5 +3,5 @@
 echo -en "Initializing website..." && \
 docker build -t website:2.0.0 . 1> /tmp/dockerbuildwebsite && \
 echo "Initialized!" && \
-docker run --rm -p 8123:8080 website:2.0.0 || \
+docker run --rm -p 8080:8080 website:2.0.0 || \
 echo "Failed to initialize!" && cat /tmp/dockerbuildwebsite
