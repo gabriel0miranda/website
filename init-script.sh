9fd9efb1f3c7a47c1a1433190a88a42aacabc27b
Gabriel Miranda <gabriel.miranda@poli.digital>
2025-07-09 14:29:19 -0300

    [FIX] Mudando de docker para podman

--- init-script.sh
+++ init-script.sh
@@ -2,7 +2,7 @@
 
 echo -en "Initializing website..." && \
 lua scripts/generate-list.lua site/public/logs/index.html site/public/logs/entries
-docker build -t website:2.0.0 . 1> /tmp/dockerbuildwebsite && \
+podman build -t website:2.0.0 . 1> /tmp/dockerbuildwebsite && \
 echo "Initialized!" && \
-docker run --rm -p 8080:8080 website:2.0.0 || \
+podman run --rm -p 8081:8080 website:2.0.0 || \
 echo "Failed to initialize!" && cat /tmp/dockerbuildwebsite
